a35bca8aaf126cdbfabd175850b8fe596583687e
johndahrah <dahrah@yandex.ru>
2019-03-27 10:32:53 +0300

    add simple JUnit tests

--- src/main/java/org/eclipse/rdf4j/examples/model/Example07WriteTurtle.java
+++ src/main/java/org/eclipse/rdf4j/examples/model/Example07WriteTurtle.java
@@ -7,15 +7,19 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.examples.model;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.eclipse.rdf4j.model.BNode;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.model.util.ModelException;
 import org.eclipse.rdf4j.model.vocabulary.FOAF;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
+import org.junit.Test;
 
 /**
  * RDF Tutorial example 07: Writing an RDF model in Turtle syntax
@@ -27,15 +31,28 @@ import org.eclipse.rdf4j.rio.Rio;
  * @see
  */
 public class Example07WriteTurtle {
-
+	
+	private ValueFactory vf;
+	private BNode address;
+	private ModelBuilder builder;
+	
 	public static void main(String[] args) {
-
+		Example07WriteTurtle testClass = new Example07WriteTurtle();
+		testClass.writeCorrectTurtle();
+		testClass.writeIncorrectTurtle();
+	}
+	
+	public Example07WriteTurtle() {
 		// To create a blank node for the address, we need a ValueFactory
-		ValueFactory vf = SimpleValueFactory.getInstance();
-		BNode address = vf.createBNode();
+		vf = SimpleValueFactory.getInstance();
+		address = vf.createBNode();
 
 		// Identically to example 03, we create a model with some data
-		ModelBuilder builder = new ModelBuilder();
+		builder = new ModelBuilder();
+	}
+
+	@Test
+	public void writeCorrectTurtle() {
 		builder
 				.setNamespace("ex", "http://example.org/")
 				.subject("ex:Picasso")
@@ -46,12 +63,26 @@ public class Example07WriteTurtle {
 					.add("ex:street", "31 Art Gallery")
 					.add("ex:city", "Madrid")
 					.add("ex:country", "Spain");
-
+		
 		Model model = builder.build();
+		
+		assertNotNull(model);
 
 		// Instead of simply printing the statements to the screen, we use a Rio writer to
 		// write the model in Turtle syntax:
 		Rio.write(model, System.out, RDFFormat.TURTLE);
-
+	}
+	
+	@Test(expected = ModelException.class)
+	public void writeIncorrectTurtle() {
+		builder
+			.setNamespace("ex", "http://example.org/")
+			// set subject name to unsupported
+			.subject("someSubject")
+				.add(RDF.TYPE, "ex:Artist")
+				.add(FOAF.FIRST_NAME, "Pablo");
+		
+		Model model = builder.build();
+		Rio.write(model, System.out, RDFFormat.TURTLE);
 	}
 }
