d005cf4d0ce62d7e2523ee0a0c88c6638b189392
ChinukB12 <troshnev@bk.ru>
2019-06-26 21:30:48 +0300

    UpdateTest

--- org.eclipse.rdf4j.jaxrs/org.eclipse.rdf4j/org.eclipse.rdf4j.jaxrs/src/test/java/com/example/myproject/test/SampleTest.java
+++ org.eclipse.rdf4j.jaxrs/org.eclipse.rdf4j/org.eclipse.rdf4j.jaxrs/src/test/java/com/example/myproject/test/SampleTest.java
@@ -1,19 +1,17 @@
 package com.example.myproject.test;
- 
-import static org.junit.Assert.assertNotNull;
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureSecurity;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
 
-import java.io.File;
+import static org.junit.Assert.*;
+import static org.ops4j.pax.exam.CoreOptions.*;
 
-import org.apache.karaf.itests.KarafTestSupport;
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.rdf4j.http.server.repository.RepositoryConfigController;
+import org.eclipse.rdf4j.http.server.transaction.ActiveTransactionRegistry;
+import org.eclipse.rdf4j.http.server.transaction.TransactionControllerUpdate;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.config.ConfigTemplate;
+import org.eclipse.rdf4j.repository.config.RepositoryConfig;
 import org.eclipse.rdf4j.repository.manager.RepositoryManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,20 +22,31 @@ import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.karaf.itests.KarafTestSupport;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class SampleTest extends KarafTestSupport {
- 
+
     //@Inject
     //private RepositoryManager manager;
- 
+
     @Override
     @Configuration
     public Option[] config() {
         MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf").versionAsInProject().type("tar.gz");
- 
+
         String httpPort = "8181";
         String rmiRegistryPort = Integer.toString(getAvailablePort(Integer.parseInt(MIN_RMI_REG_PORT), Integer.parseInt(MAX_RMI_REG_PORT)));
         String rmiServerPort = Integer.toString(getAvailablePort(Integer.parseInt(MIN_RMI_SERVER_PORT), Integer.parseInt(MAX_RMI_SERVER_PORT)));
@@ -46,7 +55,7 @@ public class SampleTest extends KarafTestSupport {
         if (localRepository == null) {
             localRepository = "";
         }
-        
+
         return new Option[] {
         	//KarafDistributionOption.debugConfiguration("8889", true),
             karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
@@ -57,7 +66,8 @@ public class SampleTest extends KarafTestSupport {
             logLevel(LogLevelOption.LogLevel.INFO),
             mavenBundle().groupId("org.awaitility").artifactId("awaitility").versionAsInProject(),
             mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
-            mavenBundle().groupId("org.apache.karaf.itests").artifactId("common").versionAsInProject(),
+                mavenBundle().groupId("org.apache.karaf.itests").artifactId("common").versionAsInProject(),
+              //  mavenBundle().groupId("org.mockito").artifactId("mockito-core").version("2.23.4"),
             features(maven().groupId("ru.agentlab.rdf4j.server")
             	.artifactId("ru.agentlab.rdf4j.server.features").type("xml")
                	.version("0.0.1-SNAPSHOT"), "org.eclipse.rdf4j.jaxrs"),
@@ -69,27 +79,80 @@ public class SampleTest extends KarafTestSupport {
             editConfigurationFilePut("etc/org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository", localRepository)
         };
     }
- 
+
     @Test
     public void getHelloService() throws Exception {
     	// installing a feature and verifying that it's correctly installed
         installAndAssertFeature("scr");
-        
+
         //installAndAssertFeature("org.eclipse.rdf4j.jaxrs");
-        
+
      // testing a command execution
         String bundles = executeCommand("bundle:list -t 0");
         System.out.println(bundles);
         assertContains("junit", bundles);
-        
+
         String features = executeCommand("feature:list -i");
         System.out.print(features);
         assertContains("scr", features);
-        
+
      // using a service and assert state or result
-        RepositoryManager manager = getOsgiService(RepositoryManager.class);        
+        RepositoryManager manager = getOsgiService(RepositoryManager.class);
         assertNotNull(manager);
+
         System.out.println("Size=" + manager.getAllRepositories().size());
         System.out.println("Location=" + manager.getLocation());
+    }
+
+    @Test
+    public void update() throws IOException {
+        executeCommand("bundle:dynamic-import 75");
+        executeCommand("bundle:dynamic-import 175");
+        RepositoryConfigController rcc = getOsgiService(RepositoryConfigController.class);
+        String repId = "id128";
+        ConfigTemplate ct = rcc.getConfigTemplate("memory");
+        System.out.println("ConfigTemplate: " + ct);
+
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("Repository ID", repId);
+        String strConfTemplate = ct.render(queryParams);
+        System.out.println("ConfigTemplate render: " + strConfTemplate);
+        System.out.println("------------------------------");
+        System.out.println(strConfTemplate);
+        RepositoryConfig rc = rcc.updateRepositoryConfig(strConfTemplate);
+
+        RepositoryManager manager = getOsgiService(RepositoryManager.class);
+        Repository repository = manager.getRepository("id128");
+        RepositoryConnection repositoryConnection = repository.getConnection();
+
+        repositoryConnection.begin();
+
+
+        String transactionId = "1234";
+        ActiveTransactionRegistry.INSTANCE.register(transactionId,repositoryConnection);
+        TransactionControllerUpdate controller = new TransactionControllerUpdate();
+
+        //HttpServletRequest req = mock(HttpServletRequest.class);
+        //when(req.getParameter("action")).thenReturn(null);
+
+        String restLocation = "http://localhost:8181/rdf4j2-server/repositories/rpo13/transactions/1234";
+        URL url = null;
+        try {
+            url = new URL(restLocation);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        HttpServletRequest connection = (HttpServletRequest) url.openConnection();
+        try {
+            controller.handleRequestInternal(connection,"rpo13",transactionId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Exception occurs");
+            repositoryConnection.close();
+        }
+
+        repositoryConnection.close();
     }
 }
