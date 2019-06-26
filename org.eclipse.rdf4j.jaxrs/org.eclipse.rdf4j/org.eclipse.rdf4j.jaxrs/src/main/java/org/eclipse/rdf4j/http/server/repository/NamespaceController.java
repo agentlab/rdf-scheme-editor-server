409628ac844f2cec51e4e3ae4309e24666a9e032
16u264 <narivnayaav@student.bmstu.ru>
2019-06-26 23:16:12 +0300

    https://github.com/agentlab/rdf-scheme-editor-server/issues/6

--- org.eclipse.rdf4j.jaxrs/org.eclipse.rdf4j/org.eclipse.rdf4j.jaxrs/src/main/java/org/eclipse/rdf4j/http/server/repository/NamespaceController.java
+++ org.eclipse.rdf4j.jaxrs/org.eclipse.rdf4j/org.eclipse.rdf4j.jaxrs/src/main/java/org/eclipse/rdf4j/http/server/repository/NamespaceController.java
@@ -9,20 +9,16 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.eclipse.rdf4j.RDF4JException;
-import org.eclipse.rdf4j.http.protocol.Protocol;
 import org.eclipse.rdf4j.http.server.ClientHTTPException;
 import org.eclipse.rdf4j.http.server.ServerHTTPException;
 import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.repository.manager.RepositoryManager;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.eclipse.rdf4j.repository.config.ConfigTemplate;
 import org.eclipse.rdf4j.repository.config.RepositoryConfig;
-import org.eclipse.rdf4j.repository.config.RepositoryConfigSchema;
 
 
 import java.io.IOException;
@@ -51,7 +47,6 @@ public class NamespaceController {
 	private void createRepoConfig(RepositoryConfigController rcc, String repId) throws RDF4JException, IOException {
 		ConfigTemplate ct = rcc.getConfigTemplate("native");
 		System.out.println("ConfigTemplate: " + ct);
-
 		Map<String, String> queryParams = new HashMap<>();
 		queryParams.put("Repository ID", repId);
 		String strConfTemplate = ct.render(queryParams);
@@ -59,84 +54,135 @@ public class NamespaceController {
 		RepositoryConfig rc = rcc.updateRepositoryConfig(strConfTemplate);
 		System.out.println("RepositoryConfig.id: " + rc.getID());
 		System.out.println("RepositoryConfig: " + rc.toString());
-		}
+	}
 
 	
 	
 	@PUT
 	@Path ("/repositories/{repId}/namespaces/{prefix}")
 	public void putNamesp (@Context UriInfo uriInfo, @PathParam("repId") String repId,
-				@PathParam("prefix") String prefix,@QueryParam("context") Resource[] context,@QueryParam("baseURI") IRI baseURI, String body)throws ClientHTTPException, ServerHTTPException, WebApplicationException, IOException {
-			Repository repository = null;
-			
-	        try {
-	        repository = repositoryManager.getRepository(repId);
-	        }
-	        catch (Exception e){
-	        	System.out.println("Не получили репозиторий из менеджера");
-	        	e.printStackTrace();
-	        }
-	        if (repository != null) {
-	        	System.out.println(repId + " найден");
-	        	repository.getConnection().setNamespace(prefix, body);
-	        	System.out.println("Создан = " + prefix + " " + body);
-	        }
-	        else {
-	        	System.out.println(repId + " не найден");
-	        	createRepoConfig(rcc, repId);
-	        	try {
-	        	repository = repositoryManager.getRepository(repId);
-	        	repository.init();
-	        	System.out.println(repId + " создан");
-	        	}
-	        	catch (Exception e){
-	        		System.out.println("Ошибка создания репозитория");
-		        	e.printStackTrace();
-	        	}
-	        	/*try {
-	        		repositoryCon.setNamespace(prefix, body);
-	        	}
-	        	catch(Exception e) {
-	        		System.out.println("Ошибка создания неймспейса");
-		        	e.printStackTrace();
-	        	}
-	        	System.out.println("Создан = " + prefix + " " + body);
-	        	repositoryCon.commit();
-	        	repositoryCon.close();
-	        	*/
-	        }
+				@PathParam("prefix") String prefix,@QueryParam("context") Resource[] context,@QueryParam("baseURI") IRI baseURI, String body)throws  
+					 WebApplicationException, IOException {
+			if (body.length()==0) {
+				System.out.println("Не получили неймспейс");
+				throw new WebApplicationException("Error create" ,Response.Status.BAD_REQUEST);
+			}
+			else {
+				Repository repository = null;
+				RepositoryConnection repositoryCon  = null;
+	  
+				try {
+						repository = repositoryManager.getRepository(repId);
+						repositoryCon = repository.getConnection();
+				}
+				catch (Exception e){
+					System.out.println("Не получили репозиторий из менеджера");
+					e.printStackTrace();
+				}
+				if (repository != null) {
+					System.out.println(repId + " найден");
+					try {
+						if (repositoryCon.getNamespace(prefix) != null) {
+							repositoryCon.setNamespace(prefix, body);
+							System.out.println("Обновлен = " + prefix + " " + repositoryCon.getNamespace(prefix));
+						}	
+						else {
+							repositoryCon.setNamespace(prefix, body);
+							System.out.println("Создан = " + prefix + " " + repositoryCon.getNamespace(prefix));
+						}
+					}
+					catch (Exception e) {
+						System.out.println("Ошибка создания неймспейса");
+						e.printStackTrace();
+					}
+				}
+				else {
+					System.out.println(repId + " не найден");
+					createRepoConfig(rcc, repId);
+					try {
+						repository = repositoryManager.getRepository(repId);
+						repository.init();
+						repositoryCon = repository.getConnection();
+						System.out.println("Репозиторий "+ repId + " создан");
+					}
+					catch (Exception e){
+						System.out.println("Ошибка создания репозитория");
+						e.printStackTrace();
+					}
+					try {
+						if (repositoryCon.getNamespace(prefix) != null) {
+							repositoryCon.setNamespace(prefix, body);
+							System.out.println("Обновлен = " + prefix + " " + repositoryCon.getNamespace(prefix));	
+						}
+						else {
+							repositoryCon.setNamespace(prefix, body);
+							System.out.println("Создан = " + prefix + " " + repositoryCon.getNamespace(prefix));
+						}	
+					}
+					catch (Exception e) {
+						System.out.println("Ошибка создания неймспейса");
+						e.printStackTrace();
+					}
+				}
+			}
 	}
 		
-	/*
+	
 	@DELETE 
 	@Path ("/repositories/{repId}/namespaces/{prefix}")
 	public void delNamesp ( @PathParam("repId") String repId,
 				@PathParam("prefix") String prefix ) throws ClientHTTPException, ServerHTTPException, IOException {
-		try {
-			repository = repositoryManager.getRepository(repId);
-		}
-		catch (Exception e) {
-				System.out.println("Не получили репозиторий из менеджера");
-				e.printStackTrace();
-		}
-			if (repository != null) {
-				repository.init();
-				RepositoryConnection repositoryCon = repository.getConnection();
-				repositoryCon.begin();
-				try {
-					repositoryCon.removeNamespace(prefix);
-					System.out.println("Remove namespace = "+ prefix);
-				}
-				catch (RepositoryException e) {
-					throw new ServerHTTPException("Repository put error: " + e.getMessage(), e);
-				}
-				repositoryCon.commit();
-				repositoryCon.close();
-			}
-			else {
-				System.out.println(repId + " не найден");
-			        
-			}
+		Repository repository = null;
+		RepositoryConnection repositoryCon  = null;
+		  
+        try {
+        	repository = repositoryManager.getRepository(repId);
+        	repositoryCon = repository.getConnection();
+        }
+        catch (Exception e){
+        	System.out.println("Не получили репозиторий из менеджера");
+        	e.printStackTrace();
+        }
+        if (repository != null) {
+        	System.out.println(repId + " найден");
+        	try {
+        		if (repositoryCon.getNamespace(prefix) != null) {
+        			repositoryCon.removeNamespace(prefix);
+        			System.out.println("Удален = " + prefix + " " + repositoryCon.getNamespace(prefix));
+        		}
+        		else System.out.println("Неймспейс не существует");
+        	}
+        	catch (Exception e) {
+        		System.out.println("Ошибка удаления неймспейса");
+        		e.printStackTrace();
+        	}
+        }
+        else {
+        	System.out.println(repId + " не найден");
+        	createRepoConfig(rcc, repId);
+        	try {
+        		repository = repositoryManager.getRepository(repId);
+        		repository.init();
+        		repositoryCon = repository.getConnection();
+        		System.out.println("Репозиторий "+ repId + " создан");
+        	}
+        	catch (Exception e){
+        		System.out.println("Ошибка создания репозитория");
+	        	e.printStackTrace();
+        	}
+        	try {
+        		if (repositoryCon.getNamespace(prefix) != null) {
+        			repositoryCon.removeNamespace(prefix);
+        			System.out.println("Удален = " + prefix + " " + repositoryCon.getNamespace(prefix));
+        		}
+        		else System.out.println("Неймспейс не существует");
+        	}
+        	catch (Exception e) {
+        		System.out.println("Ошибка удаления неймспейса");
+        		e.printStackTrace();
+        	}
+        	
+        }
 	}
-	*/	
+	
 }
