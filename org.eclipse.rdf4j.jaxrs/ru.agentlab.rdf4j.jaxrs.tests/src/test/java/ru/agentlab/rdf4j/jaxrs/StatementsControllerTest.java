51f5ac6707c0657fd85645beab2b65a879e3211b
Ваше Имя <you@example.com>
2019-10-07 16:49:10 +0300

    StatementControllTest #78

--- org.eclipse.rdf4j.jaxrs/ru.agentlab.rdf4j.jaxrs.tests/src/test/java/ru/agentlab/rdf4j/jaxrs/StatementsControllerTest.java
+++ org.eclipse.rdf4j.jaxrs/ru.agentlab.rdf4j.jaxrs.tests/src/test/java/ru/agentlab/rdf4j/jaxrs/StatementsControllerTest.java
@@ -1,18 +1,21 @@
 package ru.agentlab.rdf4j.jaxrs;
 
+import static org.eclipse.rdf4j.model.util.Models.isSubset;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Set;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.rio.RDFFormat;
@@ -24,6 +27,7 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
+import ru.agentlab.rdf4j.jaxrs.repository.RepositoryController;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
@@ -32,7 +36,7 @@ public class StatementsControllerTest extends Rdf4jJaxrsTestSupport {
     String address;
     String file = "/testcases/default-graph-1.ttl";
     RDFFormat dataFormat = Rio.getParserFormatForFileName(file).orElse(RDFFormat.RDFXML);
-    
+
     String repId = "id1237";
     Repository repository;
     RepositoryConnection repositoryCon;
@@ -44,7 +48,7 @@ public class StatementsControllerTest extends Rdf4jJaxrsTestSupport {
         repository = manager.getOrCreateRepository(repId, "native-rdfs", null);
         repositoryCon = repository.getConnection();
     }
-    
+
     @After
     public void cleanup() {
         repositoryCon.close();
@@ -52,6 +56,8 @@ public class StatementsControllerTest extends Rdf4jJaxrsTestSupport {
 
     @Test
     public void postStatementsShouldWorkOk() throws IOException {
+
+        // post statement to repository
         WebClient client = WebClient.create(address);
         client.type(dataFormat.getDefaultMIMEType());
         client.accept(MediaType.WILDCARD);
@@ -62,7 +68,30 @@ public class StatementsControllerTest extends Rdf4jJaxrsTestSupport {
         assertEquals(204, response.getStatus());
         assertEquals("", response.readEntity(String.class));
         client.close();
-        
+
         assertThat("repositoryCon.size", repositoryCon.size(), equalTo(4L));
+
+
+        // get statements and check our statement is Subset of got statements
+        WebClient client2 = WebClient.create(address);
+        client2.type(dataFormat.getDefaultMIMEType());
+        client2.accept(MediaType.WILDCARD);
+        Response response2 = client2.get();
+
+        InputStream dataStream2 = RepositoryControllerTest.class.getResourceAsStream(file);
+        Model modelOurFile = Rio.parse(dataStream2,"",RDFFormat.TURTLE);
+
+        String gotString = response2.readEntity(String.class);
+        assertEquals(200, response2.getStatus());
+        System.out.println("Response.getStatus():" + response2.getStatus());
+
+        Reader reader = new StringReader(gotString);
+        Model gotModel = Rio.parse(reader,"", RDFFormat.TURTLE);
+
+        //isSubset
+        assertTrue(isSubset(modelOurFile,gotModel));
+        System.out.println("OurFileIsSubset: " + isSubset(modelOurFile,gotModel));
+
+        client2.close();
     }
 }
