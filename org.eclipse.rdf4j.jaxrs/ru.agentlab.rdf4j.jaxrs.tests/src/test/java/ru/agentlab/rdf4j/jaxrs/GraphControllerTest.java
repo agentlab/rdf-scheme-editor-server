a8e63df62d6f4ee23f7820fab728b0183028c655
Enfaseel <Enfaseel@users.noreply.github.com>
2019-10-07 14:58:29 +0300

    Реализована программная имплементация GraphControllerTest и внесены небольшие правки в исходный тест.

--- org.eclipse.rdf4j.jaxrs/ru.agentlab.rdf4j.jaxrs.tests/src/test/java/ru/agentlab/rdf4j/jaxrs/GraphControllerTest.java
+++ org.eclipse.rdf4j.jaxrs/ru.agentlab.rdf4j.jaxrs.tests/src/test/java/ru/agentlab/rdf4j/jaxrs/GraphControllerTest.java
@@ -1,58 +1,63 @@
 package ru.agentlab.rdf4j.jaxrs;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.util.Models;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GraphControllerTest extends Rdf4jJaxrsTestSupport {
-    String ENDPOINT_ADDRESS;
-    String address;
-    String file = "/testcases/default-graph-1.ttl";
-    RDFFormat dataFormat = Rio.getParserFormatForFileName(file).orElse(RDFFormat.RDFXML);
-    
-    String repId = "id1238";
-    Repository repository;
-    RepositoryConnection repositoryCon;
+    private String address;
+    private String file = "/testcases/default-graph-1.ttl";
+    private RDFFormat dataFormat = Rio.getParserFormatForFileName(file).orElse(RDFFormat.RDFXML);
+
+    private String repId = "id1238";
+    private String graphName = "graph1";
+    private RepositoryConnection repositoryCon;
 
     @Before
     public void init() throws Exception {
-        ENDPOINT_ADDRESS = "http://localhost:" + getHttpPort() + "/rdf4j-server/repositories/";
-        address = ENDPOINT_ADDRESS + repId + "/rdf-graphs/graph1";
-        repository = manager.getOrCreateRepository(repId, "native-rdfs", null);
+        String ENDPOINT_ADDRESS = "http://localhost:" + getHttpPort() + "/rdf4j-server/repositories/";
+        String graphSection = "/rdf-graphs/";
+        address = ENDPOINT_ADDRESS + repId + graphSection + graphName;
+        Repository repository = manager.getOrCreateRepository(repId, "native-rdfs", null);
         repositoryCon = repository.getConnection();
     }
-    
+
     @After
     public void cleanup() {
         repositoryCon.close();
     }
 
     @Test
-    public void postStatementsToGraphShouldWorkOk() throws IOException {
+    public void stage1_postStatementsToGraphShouldWorkOk() throws IOException {
         System.out.println("POST statements to graph on address=" + address);
         WebClient client = WebClient.create(address);
         client.type(dataFormat.getDefaultMIMEType());
@@ -65,27 +70,39 @@ public class GraphControllerTest extends Rdf4jJaxrsTestSupport {
         System.out.println("response.body=" + response.readEntity(String.class));
         assertEquals(204, response.getStatus());
         client.close();
-
-        RepositoryConnection repositoryCon = repository.getConnection();
         assertThat("repositoryCon.size", repositoryCon.size(), equalTo(4L));
-        
+    }
+
+    @Test
+    public void stage2_shouldGetGraphByRestAPI() throws IOException {
         System.out.println("GET statements from named graph on address=" + address);
-        WebClient client2 = WebClient.create(address);
-        client2.accept(MediaType.WILDCARD);
-        Response response2 = client2.get();
-        System.out.println("response2.status=" + response2.getStatusInfo().getReasonPhrase());
-        System.out.println("response2.body=" + response2.readEntity(String.class));
-        assertEquals(200, response2.getStatus());
-        client2.close();
-        
-        /*String address = ENDPOINT_ADDRESS + repId + "/rdf-graphs/graph1";
-        System.out.println("GET statements from default graph on address=" + address2);
-        WebClient client3 = WebClient.create(address);
-        client3.accept(MediaType.WILDCARD);
-        Response response3 = client3.get();
-        System.out.println("response3.status=" + response3.getStatusInfo().getReasonPhrase());
-        System.out.println("response3.body=" + response3.readEntity(String.class));
-        assertEquals(200, response3.getStatus());
-        client3.close();*/
+        WebClient client = WebClient.create(address);
+        client.accept(MediaType.WILDCARD);
+        Response response = client.get();
+        String body = response.readEntity(String.class);
+        Reader reader = new StringReader(body);
+        Model modelFromGet = Rio.parse(reader, "", RDFFormat.RDFXML);
+        InputStream inputStream = RepositoryControllerTest.class.getResourceAsStream(file);
+        Model modelFromFile = Rio.parse(inputStream, "", dataFormat);
+
+        assertThat("File model is subset of GET model", Models.isSubset(modelFromFile, modelFromGet));
+
+        System.out.println("response.status=" + response.getStatusInfo().getReasonPhrase());
+        System.out.println("response.body=" + response.readEntity(String.class));
+        assertEquals(200, response.getStatus());
+        client.close();
+    }
+
+    @Test
+    public void stage3_shouldDeleteGraphFromRepository() {
+        System.out.println("DELETE \"" + graphName + "\" from repository\"" + repId + "\" on address=" + address);
+        WebClient client = WebClient.create(address);
+        client.accept(MediaType.WILDCARD);
+        Response response = client.delete();
+        assertThat("repositoryCon.size", repositoryCon.size(), equalTo(0L));
+        System.out.println("response.status=" + response.getStatusInfo().getReasonPhrase());
+        System.out.println("response.body=" + response.readEntity(String.class));
+        assertEquals(204, response.getStatus());
+        client.close();
     }
 }
